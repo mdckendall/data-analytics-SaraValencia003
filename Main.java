680ac189266ac17a853d1eed384274514ef18679
Sara Valencia <98754695+SaraValencia003@users.noreply.github.com>
2022-10-18 18:15:47 +0000

    n

--- Main.java
+++ Main.java
@@ -1,14 +1,55 @@
-import java.io.FileReader;
 import java.util.Scanner;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.*;
+
 
 class Main {
-	public static void main(String[] args) throws FileNotFoundException {
-
-		ArrayList<String> arrayList = new ArrayList<>();
-		FileReader fr = new FileReader("names.txt");
-		Scanner fileScanner = new Scanner(fr);
-		
-	}
+  public static void main(String[] args) {
+    
+  boolean Continue = true;
+    while (Continue == true) {
+      
+      Scanner option = new Scanner(System.in);
+      System.out.println("Press 1 to learn about salary. \nPress 2 to   learn about the job. \nPress 3 to learn about demand. \nPress 4 to view current students. \nPress 5 to quit.");
+      String choice = option.nextLine();
+      if(choice.equals("1")){
+        System.out.println("$98,345 average salary in South FLorida!");
+        
+      } if(choice.equals("2")){
+        System.out.println("US News - 100 Best Jobs!");
+                
+      }if(choice.equals("3")){
+        System.out.println("Top 10 Forbes In-Demand Jobs!");
+                
+      }if(choice.equals("4")){
+        try{
+      
+      FileReader fr = new FileReader("names.txt");
+      BufferedReader br = new BufferedReader(fr);
+      
+      String line = br.readLine();
+      
+      while(line!=null){
+        
+        System.out.println(line);
+        line = br.readLine();
+        
+      }
+      
+      br.close();
+      fr.close();
+    
+      
+    }catch(IOException e2){}
+                      
+      }if(choice.equals("5")){
+        Continue = false;
+        break;
+                
+      }
+        
+      
+     
+      
+    } 
+  }
 }

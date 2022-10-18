769d21c7cb20ec1c88e07c14422e79c4fa86f752
Sara Valencia <98754695+SaraValencia003@users.noreply.github.com>
2022-10-18 21:18:39 +0000

    .

--- Main.java
+++ Main.java
@@ -6,21 +6,25 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    
+  Scanner option = new Scanner(System.in);
+      
+      
   boolean Continue = true;
     while (Continue == true) {
-      
-      Scanner option = new Scanner(System.in);
       System.out.println("Press 1 to learn about salary. \nPress 2 to   learn about the job. \nPress 3 to learn about demand. \nPress 4 to view current students. \nPress 5 to quit.");
-      String choice = option.nextLine();
+  String choice = option.nextLine();
+      
       if(choice.equals("1")){
         System.out.println("$98,345 average salary in South Florida!");
         
+        
       } if(choice.equals("2")){
         System.out.println("US News - 100 Best Jobs!");
+      
                 
       }if(choice.equals("3")){
         System.out.println("Top 10 Forbes In-Demand Jobs!");
+        
                 
       }if(choice.equals("4")){
         try{
